7ce0a88050b3ca9ac4512499294eb0169ea9c205
Michal Gladysz <michal.gladysz@hotmail.com>
2021-02-23 11:44:02 +0100

    set user role to one specified in product option, also fixes #3 and fixes #4

--- inc/class-change-role.php
+++ inc/class-change-role.php
@@ -15,21 +15,29 @@ class Change_Role implements Component_Interface {
 	/**
 	 * Change customer role after checkout was proccesed.
 	 *
-	 * @param order_id    $order_id  ID of a created order
-	 * @param $posted_data $posted_data [description]
-	 * @param order       $order  Created order object
+	 * @param order_id    $order_id  ID of a created order.
+	 * @param $posted_data $posted_data [description].
+	 * @param order       $order  Created order object.
 	 */
 	public function change_role_on_checkout( $order_id, $posted_data, $order ) {
 
 		$items = $order->get_items();
 		$user = $order->get_user();
 
-		// Array with products IDs that we want to associate with some role.
-		$products_to_check = array( '11', '10' );
-
 		foreach ( $items as $item ) {
-			if ( $user && in_array( $item['product_id'], $products_to_check ) ) {
-				$user->set_role( 'editor' );
+			$product_id = $item->get_product_id();
+
+			$value = get_post_meta( $product_id, '_related_role', true );
+			if ( empty( $value ) ) {
+				// Exit the loop when product has no role assigned.
+				break;
+			}
+
+			if ( $user ) {
+				$user->set_role( $value );
+				$user->save();
+
+				// Exit the loop when new role has been set.
 				break;
 			}
 		}
